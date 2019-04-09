a98f6c2cdd5a45fde6d1b81119708459544c1388
grzegorz-zapala <zapala.grzegorz@gmail.com>
2019-04-09 15:58:12 +0200

    spreading

--- scripts.js
+++ scripts.js
@@ -3,36 +3,44 @@ window.addEventListener('DOMContentLoaded', function () {
     console.log('test');
 
     let simpleLevelPlan = `
-        ......................
-        ..#................#..
-        ..#..............=.#..
-        ..#.........o.o....#..
-        ..#.@......#####...#..
-        ..#####............#..
-        ......#++++++++++++#..
-        ......##############..
-        ......................`;
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
 
     class Level {
         constructor (plan) {
             let rows = plan
                 .trim()
+                // split da nam tablicę 9 elementową, po rzędzie
                 .split('\n')
-            // each line is spread into an array, producing arrays of characters.
-                .map(l => [...l]);
+                // each line (array) is spread into an array, producing arrays of characters.
+                .map(element => [...element]);
+
+            // let rows = rowstemp.map(element => {
+            //     return [...element];
+            //     // return [tempString];
+            // });
+            // let rows = rowstemp.map(element => [...element]);
+
             this.height = rows.length;
             this.width = rows[0].length;
             this.startActors = [];
 
             this.rows = rows.map((row, y) => {
                 return row.map((ch, x) => {
-                // let type = levelChars[ch];
-                // if (typeof type === 'string') return type;
-                // this.startActors.push(type.create(new Vec(x, y), ch));
+                    // let type = levelChars[ch];
+                    // if (typeof type === 'string') return type;
+                    // this.startActors.push(type.create(new Vec(x, y), ch));
                     return 'empty';
                 });
             });
         }
     }
-    Level(simpleLevelPlan);
+    new Level(simpleLevelPlan);
 });
